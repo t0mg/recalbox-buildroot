f2e42271fab4803dc13573271a536c3bda47e690
t0m <tom@t0m.fr>
2015-10-18 20:54:21 +0200

    Commenting stuff out

--- package/controlblock/controlblock.mk
+++ package/controlblock/controlblock.mk
@@ -9,32 +9,20 @@ CONTROLBLOCK_SOURCE = ControlBlockService-$(CONTROLBLOCK_VERSION).tar.gz
 CONTROLBLOCK_SITE = https://github.com/t0mg/ControlBlockService/releases/download/$(CONTROLBLOCK_VERSION)
 CONTROLBLOCK_DEPENDENCIES = libbcm2835 jsoncpp i2c-tools python-smbus
 
-define CONTROLBLOCK_POST_INSTALL_INSTALLSERVICE
-	$(TARGET_MAKE_ENV) $(MAKE) installservice -C $(@D)
-endef
+#define CONTROLBLOCK_POST_INSTALL_INSTALLSERVICE
+#	$(TARGET_MAKE_ENV) $(MAKE) installservice -C $(@D)
+#endef
 
-CONTROLBLOCK_POST_INSTALL_TARGET_HOOKS += CONTROLBLOCK_POST_INSTALL_INSTALLSERVICE
+#CONTROLBLOCK_POST_INSTALL_TARGET_HOOKS += CONTROLBLOCK_POST_INSTALL_INSTALLSERVICE
 
-define CONTROLBLOCK_CONFIGURE_CMDS
-	(cd $(@D); rm -rf config.cache; \
-		$(TARGET_CONFIGURE_ARGS) \
-		$(TARGET_CONFIGURE_OPTS) \
-		CFLAGS="$(TARGET_CFLAGS)" \
-		LDFLAGS="$(TARGET_LDFLAGS) -lc" \
-		CROSS_COMPILE="$(HOST_DIR)/usr/bin/" \
-		./configure \
-		--prefix=/usr \
-	)
-endef
-
-define CONTROLBLOCK_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
-endef
+#define CONTROLBLOCK_BUILD_CMDS
+#	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+#endef
 
 define CONTROLBLOCK_INSTALL_TARGET_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) installservice -C $(@D)
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
 	#$(MAKE) -C $(@D) install INSTALL_ROOT=$(TARGET_DIR)
-  	#$(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) install
+  	$(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) install
 endef
 
 $(eval $(generic-package))
